4a9d52a2290e2cdabe4077080d080c9f28af9479
kevinfiol <keb@protonmail.com>
2019-02-07 19:00:20 -0500

    minor formatting updates

--- app/App.php
+++ app/App.php
@@ -25,7 +25,7 @@ if (file_exists($configPath) && is_readable($configPath)) {
 /**
  * Create Dependencies
  */
-$container  = new Container();
+$container          = new Container();
 $createDependencies = include_once __DIR__ . '/Dependencies.php';
 $dependencies       = $createDependencies($config);
 
